b7ddad671596eeea0e6882804c58369579cb151b
PGM <pedro13087@gmail.com>
2026-03-15 22:31:23 +0100

    Update FallbackChannels.kt: Remove several TV and radio channels
    - Remove national channels: Trece, Ten, Real Madrid TV
    - Remove regional channels: Canal Sur Andalucía, Telemadrid, TVG Galicia, IB3 Baleares
    - Remove Radio 3 from music category

--- app/src/main/java/com/pedrogm/tdtflow/data/FallbackChannels.kt
+++ app/src/main/java/com/pedrogm/tdtflow/data/FallbackChannels.kt
@@ -44,18 +44,7 @@ internal fun fallbackChannels(): List<Channel> = createChannels {
     channel("Clan", dvr("clan"), ChannelCategory.KIDS, fbLogo("clantve"))
     channel("Teledeporte", dvr("tdp"), ChannelCategory.SPORTS, fbLogo("teledeporteRTVE"))
 
-    // Nacionales libres
-    channel("Trece", "http://176.65.146.237:8401/play/a0a9/index.m3u8", ChannelCategory.GENERAL, fbLogo("TRECEtves"))
-    channel("Ten", "https://unlimited1-us.dps.live/ten/ten.smil/playlist.m3u8", ChannelCategory.GENERAL, fbLogo("TenTV"))
-    channel("Real Madrid TV", "https://rmtv.akamaized.net/hls/live/2043153/rmtv-es-web/master.m3u8", ChannelCategory.SPORTS, fbLogo("RealMadrid"))
 
-    // Andalucía
-    channel(
-        "Canal Sur Andalucía",
-        "https://d35x6iaiw8f75z.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-kbwsl0jk1bvoo/canal_sur_andalucia_es.m3u8",
-        ChannelCategory.GENERAL,
-        fbLogo("canalsurradioytv")
-    )
     channel(
         "Canal Sur 2",
         "https://cdnlive.codev8.net/rtvalive/smil:channel22.smil/playlist.m3u8",
@@ -63,13 +52,7 @@ internal fun fallbackChannels(): List<Channel> = createChannels {
         fbLogo("canalsurradioytv")
     )
 
-    // Madrid
-    channel(
-        "Telemadrid",
-        "https://telemadrid-23-secure2.akamaized.net/master.m3u8",
-        ChannelCategory.GENERAL,
-        fbLogo("telemadrid")
-    )
+
 
     // Cataluña
     channel(
@@ -87,13 +70,6 @@ internal fun fallbackChannels(): List<Channel> = createChannels {
         fbLogo("eitb")
     )
 
-    // Galicia
-    channel(
-        "TVG Galicia",
-        "https://crtvg-galicia-schlive.flumotion.cloud/crtvglive/smil:channel1PRG.smil/playlist.m3u8",
-        ChannelCategory.GENERAL,
-        fbLogo("TelevisionGalicia")
-    )
 
     // Aragón
     channel(
@@ -103,18 +79,9 @@ internal fun fallbackChannels(): List<Channel> = createChannels {
         fbLogo("aragontelevision")
     )
 
-    // Baleares
-    channel(
-        "IB3 Baleares",
-        "http://ibsatiphone.ib3tv.com/iphoneliveIB3/IB3/bitrate_2.m3u8",
-        ChannelCategory.GENERAL,
-        fbLogo("IB3TV")
-    )
-
     // RADIO / MÚSICA - Servidores estables 2025
     channel("LOS40", "https://playerservices.streamtheworld.com/api/livestream-redirect/LOS40.mp3", ChannelCategory.MUSIC, fbLogo("los40"))
     channel("Rock FM", "https://rockfm-cope-rrcast.flumotion.com/cope/net1.mp3", ChannelCategory.MUSIC, fbLogo("rockfm"))
     channel("Cadena SER", "https://playerservices.streamtheworld.com/api/livestream-redirect/CADENASER.mp3", ChannelCategory.MUSIC, fbLogo("cadenaser"))
-    channel("Radio 3", "https://rtvelivestream.akamaized.net/rtvesec/rne/rne_r3_main.m3u8", ChannelCategory.MUSIC, fbLogo("radio3tve"))
     channel("Kiss FM", "https://kissfm.kissfmradio.cires21.com/kissfm.mp3", ChannelCategory.MUSIC, fbLogo("KissFMSpain"))
 }
