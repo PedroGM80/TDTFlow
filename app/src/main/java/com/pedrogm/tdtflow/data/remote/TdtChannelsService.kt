f87cf40aa894ae89a014c94a708564683cbf01d0
PGM <pedro13087@gmail.com>
2026-03-17 00:20:32 +0100

    refactor: remove redundant files from app module

--- app/src/main/java/com/pedrogm/tdtflow/data/remote/TdtChannelsService.kt
+++
@@ -1,9 +0,0 @@
-package com.pedrogm.tdtflow.data.remote
-
-import retrofit2.http.GET
-
-interface TdtChannelsService {
-
-    @GET("lists/tv.json")
-    suspend fun getChannels(): TdtChannelsResponse
-}
