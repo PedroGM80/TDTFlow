46cf1aa5263806ed87f6feac84bc1f5b8b69f854
PGM <pedro13087@gmail.com>
2026-03-17 00:14:17 +0100

    refactor: make domain module a pure Kotlin library

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,24 +1,15 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.serialization)
+    id("java-library")
+    kotlin("jvm")
+    kotlin("plugin.serialization")
 }
 
-android {
-    namespace = "com.pedrogm.tdtflow.domain"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
 }
