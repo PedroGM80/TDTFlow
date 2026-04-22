98996dc635bae047ac2b33c4dfa8d2167334a3b3
pedrogallegomorales <pedro13087@gmail.com>
2026-04-22 21:12:17 +0200

    build: bump version to 1.1.0
    - Increment versionCode to 3
    - Update versionName to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         applicationId = "com.pedrogm.tdtflow"
         minSdk = androidMinSdk.toInt()
         targetSdk = androidTargetSdk.toInt()
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
     }
 
     signingConfigs {
